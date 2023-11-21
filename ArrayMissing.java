408e060b71f1c3cfd3115211bdc3e46d7c633661
PrajwalArkachari <115942847+PrajwalArkachari@users.noreply.github.com>
2023-11-21 19:00:39 +0530

    Update ArrayMissing.java

--- ArrayMissing.java
+++ ArrayMissing.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class ArrayMissing {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the n value");
 		int n= sc.nextInt();
 		int[] arr =new int[n];
-		int temp,miss;
+		int temp;
+		System.out.println("Enter the array");
 		for(int i=0;i<n;i++)
 		{
 			arr[i]=sc.nextInt();
 		}
+		System.out.println("Entered array is");
 		for(int i:arr)
 			System.out.println(i);
 		for(int i=0;i<n-1;i++)
